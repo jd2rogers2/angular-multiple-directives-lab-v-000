26428a731fc162880d27ab73b494b6953dd6d92f
jd2rogers2 <jd2rogers2@gmail.com>
2016-10-28 17:31:21 -0400

    donzo

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,7 @@
 function ContactController() {
-    this.contacts = [{
+    var vm = this;
+
+    vm.contacts = [{
         name: 'Bob',
         phone: '0123458690'
     },{
@@ -10,8 +12,14 @@ function ContactController() {
         phone: '0684059433'
     }];
 
-    this.removeContact = function (index) {
-        this.contacts.splice(index, 1);
+    vm.removeContact = function (index) {
+        vm.contacts.splice(index, 1);
+    }
+
+    vm.pushToArray = function(){
+        vm.contacts.push({name: vm.name, phone: vm.phoneNumber});
+        vm.name = '';
+        vm.phoneNumber = '';
     }
 }
 
